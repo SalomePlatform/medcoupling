76315ff48d3cd2a20b107ed3a5835eea756221be
abd <abd@opencascade.com>
2008-11-26 14:50:41 +0000

    Update copyright information

--- doc/MEDMEM/FIELDcreate.py
+++ doc/MEDMEM/FIELDcreate.py
@@ -1,13 +1,30 @@
-# Copyright (C) 2005  OPEN CASCADE, CEA, EDF R&D, LEG
-#           PRINCIPIA R&D, EADS CCR, Lip6, BV, CEDRAT
-# 
+#  Copyright (C) 2007-2008  CEA/DEN, EDF R&D, OPEN CASCADE
+#
+#  Copyright (C) 2003-2007  OPEN CASCADE, EADS/CCR, LIP6, CEA/DEN,
+#  CEDRAT, EDF R&D, LEG, PRINCIPIA R&D, BUREAU VERITAS
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#  See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
 ######################################################################
-#                                                                    #
 # This Python script should be executed when the shared library is   #
 # generated using SWIG 1.3 (or higher) due to the fact that older    #
 # version could not handle the wrapping of several class constructor #
-#                                                                    #
 ######################################################################
+#
 from libMEDMEM_Swig import *
 
 MedFile = "pointe.med"
