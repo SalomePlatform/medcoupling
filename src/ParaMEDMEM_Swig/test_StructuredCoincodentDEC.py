e5745d8f278a40430de090fe5a03c3b09ca18ddb
ageay <ageay>
2010-02-02 09:37:46 +0000

    *** empty log message ***

--- src/ParaMEDMEM_Swig/test_StructuredCoincodentDEC.py
+++ src/ParaMEDMEM_Swig/test_StructuredCoincodentDEC.py
@@ -67,6 +67,7 @@ class ParaMEDMEMBasicsTest2(unittest.TestCase):
             paramesh=ParaMESH(mesh,source_group,"source mesh")
             comptopo=ComponentTopology(6)
             parafield=ParaFIELD(ON_CELLS,NO_TIME,paramesh,comptopo)
+            parafield.getField().setNature(ConservativeVolumic)
             nb_local=mesh.getNumberOfCells()
             global_numbering=paramesh.getGlobalNumberingCell2()
             value = []
@@ -76,9 +77,9 @@ class ParaMEDMEMBasicsTest2(unittest.TestCase):
                     pass
                 pass
             parafield.getField().setValues(value)
-            icocofield = ICoCoMEDField(paramesh,parafield)
+            icocofield = ICoCoMEDField(mesh,parafield.getField())
             dec.setMethod("P0")  
-            dec.attachLocalField(icocofield)      
+            dec.attachLocalField(parafield)      
             dec.synchronize()
             dec.sendData()
             pass
@@ -89,12 +90,13 @@ class ParaMEDMEMBasicsTest2(unittest.TestCase):
             paramesh=ParaMESH(mesh, self_group, "target mesh")
             comptopo=ComponentTopology(6,target_group)
             parafield=ParaFIELD(ON_CELLS,NO_TIME,paramesh, comptopo)
+            parafield.getField().setNature(ConservativeVolumic)
             nb_local=mesh.getNumberOfCells()
             value = [0.0]*(nb_local*comptopo.nbLocalComponents())
             parafield.getField().setValues(value)
-            icocofield = ICoCoMEDField(paramesh,parafield)
+            icocofield = ICoCoMEDField(mesh,parafield.getField())
             dec.setMethod("P0")
-            dec.attachLocalField(icocofield)
+            dec.attachLocalField(parafield)
             dec.synchronize()
             dec.recvData()
             recv_value = parafield.getField().getArray().getValues()
