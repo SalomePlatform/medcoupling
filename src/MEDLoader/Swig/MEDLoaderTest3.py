85a48931b71fed68749dc27bd324d6def353b0f3
ageay <ageay>
2011-04-18 17:12:29 +0000

    MEDFileField in action.

--- src/MEDLoader/Swig/MEDLoaderTest3.py
+++ src/MEDLoader/Swig/MEDLoaderTest3.py
@@ -307,32 +307,46 @@ class MEDLoaderTest(unittest.TestCase):
         pass
 
     def testMEDMesh7(self):
-       fileName="Pyfile24.med"
-       m2,m1,m0,f2,f1,f0,p,n2,n1,n0,fns,fids,grpns,famIdsPerGrp=MEDLoaderDataForTest.buildMultiLevelMesh_1()
-       m=MEDFileUMesh.New()
-       m.setCoords(m2.getCoords())
-       m.setMeshAtLevel(0,m2)
-       m.setMeshAtLevel(-1,m1)
-       m.setMeshAtLevel(-2,m0)
-       m.setFamilyFieldArr(0,f2)
-       m.setFamilyFieldArr(-1,f1)
-       m.setFamilyFieldArr(-2,f0)
-       m.setFamilyFieldArr(1,p)
-       m.setRenumFieldArr(0,n2)
-       m.setRenumFieldArr(-1,n1)
-       m.setRenumFieldArr(-2,n0)
-       nbOfFams=len(fns)
-       for i in xrange(nbOfFams):
-           m.addFamily(fns[i],fids[i])
-           pass
-       nbOfGrps=len(grpns)
-       for i in xrange(nbOfGrps):
-           m.setFamiliesIdsOnGroup(grpns[i],famIdsPerGrp[i])
-           pass
-       m.setName(m2.getName())
-       m.setDescription(m2.getDescription())
-       m.write(fileName,2)
-       pass
+        fileName="Pyfile24.med"
+        m2,m1,m0,f2,f1,f0,p,n2,n1,n0,fns,fids,grpns,famIdsPerGrp=MEDLoaderDataForTest.buildMultiLevelMesh_1()
+        m=MEDFileUMesh.New()
+        m.setCoords(m2.getCoords())
+        m.setMeshAtLevel(0,m2)
+        m.setMeshAtLevel(-1,m1)
+        m.setMeshAtLevel(-2,m0)
+        m.setFamilyFieldArr(0,f2)
+        m.setFamilyFieldArr(-1,f1)
+        m.setFamilyFieldArr(-2,f0)
+        m.setFamilyFieldArr(1,p)
+        m.setRenumFieldArr(0,n2)
+        m.setRenumFieldArr(-1,n1)
+        m.setRenumFieldArr(-2,n0)
+        nbOfFams=len(fns)
+        for i in xrange(nbOfFams):
+            m.addFamily(fns[i],fids[i])
+            pass
+        nbOfGrps=len(grpns)
+        for i in xrange(nbOfGrps):
+            m.setFamiliesIdsOnGroup(grpns[i],famIdsPerGrp[i])
+            pass
+        m.setName(m2.getName())
+        m.setDescription(m2.getDescription())
+        m.write(fileName,2)
+        pass
+   
+    def testMEDField1(self):
+        mm=MEDFileMesh.New("Pyfile17.med")
+        mm.write("Pyfile17_bis.med",2)
+        ff=MEDFileFieldMultiTS.New("Pyfile17.med","MeasureOfMesh_Extruded")
+        ff.write("Pyfile17_bis.med",0)
+        pass
+    
+    def testMEDField2(self):
+        mm=MEDFileMesh.New("Pyfile19.med")
+        mm.write("Pyfile19_bis.med",2)
+        ff=MEDFileFieldMultiTS.New("Pyfile19.med","VFieldOnNodes")
+        ff.write("Pyfile19_bis.med",0)
+        pass
     pass
 
 unittest.main()
