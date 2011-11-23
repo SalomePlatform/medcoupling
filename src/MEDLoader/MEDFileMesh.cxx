b9b68d835cbff131e184c2bfa9be4c3f732ae6a1
ageay <ageay>
2011-11-23 15:39:33 +0000

    correction of a big bug 2.

--- src/MEDLoader/MEDFileMesh.cxx
+++ src/MEDLoader/MEDFileMesh.cxx
@@ -422,6 +422,13 @@ void MEDFileMesh::changeFamilyName(const char *oldName, const char *newName) thr
   int cpy=(*it).second;
   _families.erase(it);
   _families[newName]=cpy;
+  for(std::map<std::string, std::vector<std::string> >::iterator it3=_groups.begin();it3!=_groups.end();it3++)
+    {
+      std::vector<std::string>& v=(*it3).second;
+      std::vector<std::string>::iterator it4=std::find(v.begin(),v.end(),oname);
+      if(it4!=v.end())
+        (*it4)=nname;
+    }
 }
 
 bool MEDFileMesh::areFamsEqual(const MEDFileMesh *other, std::string& what) const
