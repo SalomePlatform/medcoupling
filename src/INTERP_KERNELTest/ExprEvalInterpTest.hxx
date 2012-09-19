dd903fd5d08ad8b2eb84a8e8d0a1ccea71b08e11
ageay <ageay>
2012-09-19 09:07:35 +0000

    Add author

--- src/INTERP_KERNELTest/ExprEvalInterpTest.hxx
+++ src/INTERP_KERNELTest/ExprEvalInterpTest.hxx
@@ -16,6 +16,7 @@
 //
 // See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 //
+// Author : Anthony Geay (CEA/DEN)
 
 #ifndef _EXPREVALINTERPTEST_HXX_
 #define _EXPREVALINTERPTEST_HXX_
