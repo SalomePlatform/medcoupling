f10e2635640ffc01c9f9bc1240dc931e31fd496b
Anthony Geay <anthony.geay@edf.fr>
2016-01-20 10:24:39 +0100

    End of namespace modif.

--- v8_work/ModifyNamespace.py
+++ v8_work/ModifyNamespace.py
@@ -1,5 +1,5 @@
 import os
-#rep=("namespace ParaMEDMEM","namespace MEDCoupling")
+rep=("namespace ParaMEDMEM","namespace MEDCoupling")
 rep=("ParaMEDMEM::","MEDCoupling::")
 #rep=("ParaMEDMEMImpl::","MEDCouplingImpl::")
 
@@ -27,14 +27,14 @@ def rep1(dirname,rep):
         i+=rep0(fi2,rep)
     return i
 
-dirs=["MEDCoupling","MEDCoupling/Test","MEDLoader","MEDLoader/Swig","MEDLoader/Test","MEDPartitioner","MEDPartitioner/Test","MEDPartitioner_Swig","RENUMBER","RENUMBER_Swig","INTERP_KERNELTest","ParaMEDMEM","ParaMEDLoader","ParaMEDMEMTest","ParaMEDMEM_Swig","doc/user/doxygen/fakesources","doc/user/doxygen/doxy2swig","doc/user/doxygen/doxfiles"]
+dirs=["MEDCoupling","MEDCoupling/Test","MEDLoader","MEDLoader/Swig","MEDLoader/Test","MEDPartitioner","MEDPartitioner/Test","MEDPartitioner_Swig","RENUMBER","RENUMBER_Swig","INTERP_KERNELTest","ParaMEDMEM","ParaMEDLoader","ParaMEDMEMTest","ParaMEDMEM_Swig","doc/user/doxygen/fakesources","doc/user/doxygen/doxy2swig","doc/user/doxygen/doxfiles","/home/H87074/salome/DEV/modules/src/MED/src/MEDCouplingCorba","/home/H87074/salome/DEV/modules/src/MED/src/MEDCouplingCorba/Client","/home/H87074/salome/DEV/modules/src/MED/src/MEDCouplingCorba/Test","/home/H87074/salome/DEV/modules/src/MED/src/MEDCalc/cmp","/home/H87074/salome/DEV/modules/src/MED/src/MEDCalculator","/home/H87074/salome/DEV/modules/src/MED/src/MEDCalculator/Swig","/home/H87074/salome/DEV/modules/src/MED/src/MEDCalculator/Test","/home/H87074/salome/DEV/modules/src/PARAVIS/src/Plugins/MEDReader/IO"]
 dirname=dirs[-1]
 i=0
-
-for r,dirs,fis in os.walk(dirname):
+print rep1(dirname,rep)
+"""for r,dirs,fis in os.walk(dirname):
     for fi in fis:
         if os.path.splitext(fi)[1] not in [".dox",".doxy"]:
             continue
         i+=rep0(os.path.join(r,fi),rep)
 
-print i
+print i"""
