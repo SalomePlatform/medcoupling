58ffcef9d33d7cd15897914f603ba8ba9c88ab6f
Anthony Geay <anthony.geay@edf.fr>
2017-05-26 21:57:15 +0200

    Oops missing important file

---
+++ src/MEDCoupling/MCType.hxx
@@ -0,0 +1,32 @@
+// Copyright (C) 2017  CEA/DEN, EDF R&D
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
+// Author : Anthony Geay (EDF R&D)
+
+#ifndef __MEDCOUPLING_MCTYPE_HXX__
+#define __MEDCOUPLING_MCTYPE_HXX__
+
+typedef long Int64;
+
+typedef int Int32;
+
+#define DataArrayInt DataArrayInt32
+
+#define DataArrayIntIterator DataArrayInt32Iterator
+
+#endif
